959377639c441561994a2633fa55a1315964b129
mikefab <mikefabrikant@gmail.com>
2015-02-18 12:50:26 +0000

    Only allow event create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,33 @@
 class EventsController < ApplicationController
   respond_to :json
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :return_home, except: [:create]
   skip_before_filter  :verify_authenticity_token
 
-  # GET /events
-  # GET /events.json
-  def index
-    @events = Event.all
+
+  def return_home
+    redirect_to '/'
   end
 
-  # GET /events/1
-  # GET /events/1.json
-  def show
-  end
+  # # GET /events
+  # # GET /events.json
+  # def index
+  #   @events = Event.all
+  # end
 
-  # GET /events/new
-  def new
-    @event = Event.new
-  end
+  # # GET /events/1
+  # # GET /events/1.json
+  # def show
+  # end
 
-  # GET /events/1/edit
-  def edit
-  end
+  # # GET /events/new
+  # def new
+  #   @event = Event.new
+  # end
+
+  # # GET /events/1/edit
+  # def edit
+  # end
 
   # POST /events
   # POST /events.json
@@ -122,29 +128,29 @@ class EventsController < ApplicationController
     # end
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /events/1
+  # # PATCH/PUT /events/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @event.update(event_params)
+  #       format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @event }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @event.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /events/1
+  # # DELETE /events/1.json
+  # def destroy
+  #   @event.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
