a7dd506e1b45d092b6f845c6897a49f7adac3d10
mikefab <mikefabrikant@gmail.com>
2015-03-05 18:56:03 +0000

    strip responses

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -46,7 +46,7 @@ class Completion
               if !node2response[s.node]
                 node2response[s.node] = {}
               end
-              resp = s.text.downcase
+              resp = s.text.downcase.strip
               if resp.split.size == 1
                 resp.sub!(/[!.?]$/,'')
               end
