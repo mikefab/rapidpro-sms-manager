a51b153cd1d6b988c494bf77a08f89b8e5c06c40
mikefab <mikefabrikant@gmail.com>
2015-03-06 12:19:35 +0000

    created_at

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,17 +1,18 @@
 class Record
   include Mongoid::Document
-  field :run,       type: Integer
-  field :step,      type: String
-  field :phone,     type: String
-  field :text,      type: String
-  field :flow,      type: Integer
-  field :relayer,   type: BigDecimal 
-  field :channel,   type: BigDecimal
-  field :values,    type: Array
-  field :primary,   type: String
-  field :ids,       type: Array
+  field :run,        type: Integer
+  field :step,       type: String
+  field :phone,      type: String
+  field :text,       type: String
+  field :flow,       type: Integer
+  field :relayer,    type: BigDecimal 
+  field :channel,    type: BigDecimal
+  field :values,     type: Array
+  field :primary,    type: String
+  field :ids,        type: Array
   field :arrived_on, type: DateTime
-  field :left_on,     type: DateTime
+  field :left_on,    type: DateTime
+  field :created_at, type: DateTime
 
   # field :steps,   type: Array
   
@@ -22,11 +23,13 @@ class Record
   end
 
   def self.last_event_date(id)
-    Record.where(primary: id).last.arrived_on.strftime('%c')
+    record = Record.where(primary: id).last
+    return record.created_at ? record.created_at.strftime('%c') : record.arrived_on.strftime('%c')
   end
 
   def self.first_event_date(id)
-    Record.where(primary: id).first.arrived_on.strftime('%c')
+    record = Record.where(primary: id).first
+    return record.created_at ? record.created_at.strftime('%c') : record.arrived_on.strftime('%c')
   end
 
 
