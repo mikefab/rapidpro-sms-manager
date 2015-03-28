9fb69f0d4c9fa8b6b46b65bbc5c8759023e70ec2
mikefab <mikefabrikant@gmail.com>
2015-03-28 17:57:08 +0000

    Glyphicons issue on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
