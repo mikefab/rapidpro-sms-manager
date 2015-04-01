396860f3ab14d55de96571029f8251a877475b02
mikefab <mikefabrikant@gmail.com>
2015-04-01 09:10:25 +0000

    ecap only

--- app/controllers/api/surveys_controller.rb
+++ app/controllers/api/surveys_controller.rb
@@ -5,14 +5,17 @@ class Api::SurveysController < ApplicationController
     # Track.create(ip: request.remote_ip, params: params)
     @primary_nodes = []
     Record.all.order_by(:arrived_on => 'desc').group_by{|e| e.primary}.keys.each do |e|
-      #@primary_nodes[e] = Record.primary_node_text(e)
-      @primary_nodes << {
-        node: e,
-        question:   Record.primary_node_text(e),
-        first_date: Record.first_event_date(e), 
-        last_date:  Record.last_event_date(e),
-        total_instances:  Completion.total_instances(e)
-      }
+      unless Record.primary_node_text(e).match(/DeySay system/)
+
+        #@primary_nodes[e] = Record.primary_node_text(e)
+        @primary_nodes << {
+          node: e,
+          question:   Record.primary_node_text(e),
+          first_date: Record.first_event_date(e), 
+          last_date:  Record.last_event_date(e),
+          total_instances:  Completion.total_instances(e)
+        }
+      end
     end
 
     render json: @primary_nodes
