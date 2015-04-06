dc2d8f18f607b74fd3fcaeb49cc3543fb4bad256
mikefab <mikefabrikant@gmail.com>
2015-04-06 13:30:48 +0000

    Change app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_rapidpro-rumor-app_session'
+Rails.application.config.session_store :cookie_store, key: '_rapidpro-sms-manager_session'
