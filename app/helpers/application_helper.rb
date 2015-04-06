5553b7e0cd01eb64fe5dec02d706059fd40e824c
mikefab <mikefabrikant@gmail.com>
2015-04-06 13:30:25 +0000

    basic navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,13 @@ module ApplicationHelper
     end
   end
 
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : ''
+
+    content_tag(:span, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 
 end
