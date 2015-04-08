f3999d3e0dbc68066f3c50a1a8f27f0c63b34a82
mikefab <mikefabrikant@gmail.com>
2015-04-08 14:48:40 +0000

    Remove 'deysay' filter

--- app/controllers/api/surveys_controller.rb
+++ app/controllers/api/surveys_controller.rb
@@ -4,16 +4,14 @@ class Api::SurveysController < ApplicationController
   def index
     # Track.create(ip: request.remote_ip, params: params)
     @primary_nodes = []
-    Completion.all.order_by(:arrived_on => 'desc').group_by{|e| e.primary}.keys.each do |e|
-      unless Completion.primary_node_text(e).match(/DeySay system/)
-        @primary_nodes << {
-          node: e,
-          question:         Completion.primary_node_text(e),
-          first_date:       Completion.first_event_date(e), 
-          last_date:        Completion.last_event_date(e),
-          total_instances:  Completion.total_instances(e)
-        }
-      end
+    Completion.non_rumors.all.order_by(:arrived_on => 'desc').group_by{|e| e.primary}.keys.each do |e|
+      @primary_nodes << {
+        node: e,
+        question:         Completion.primary_node_text(e),
+        first_date:       Completion.first_event_date(e), 
+        last_date:        Completion.last_event_date(e),
+        total_instances:  Completion.total_instances(e)
+      }
     end
     render json: @primary_nodes
   end
